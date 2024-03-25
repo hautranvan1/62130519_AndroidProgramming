8d3bab1fd79a556efc1e5b0450991b1573b8c1e4
Trần Văn Hậu <hautran192002@gmail.com>
2024-03-25 22:37:07 +0700

    tạo lưu trữ dữ liệu nhập

--- ThiGK_TranVanHau_62130519/Cau1_AppBMI/src/ntu_62130519/MangHinhTinhToan.java
+++ ThiGK_TranVanHau_62130519/Cau1_AppBMI/src/ntu_62130519/MangHinhTinhToan.java
@@ -5,38 +5,58 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class MangHinhTinhToan extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtWeight;
+	private JTextField txtHeight;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MangHinhTinhToan frame = new MangHinhTinhToan();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public MangHinhTinhToan() {
+		setTitle("Chương trình tính toán BMI");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nhập cân nặng:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel.setBounds(39, 49, 96, 28);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNhpChiuCao = new JLabel("Nhập chiều cao:");
+		lblNhpChiuCao.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNhpChiuCao.setBounds(39, 101, 96, 28);
+		contentPane.add(lblNhpChiuCao);
+		
+		JLabel lblNhpThngTin = new JLabel("NHẬP THÔNG TIN ĐỂ TÍNH BMI");
+		lblNhpThngTin.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNhpThngTin.setBounds(119, 10, 191, 28);
+		contentPane.add(lblNhpThngTin);
+		
+		txtWeight = new JTextField();
+		txtWeight.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 12));
+		txtWeight.setBounds(142, 50, 191, 28);
+		contentPane.add(txtWeight);
+		txtWeight.setColumns(10);
+		
+		txtHeight = new JTextField();
+		txtHeight.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 12));
+		txtHeight.setColumns(10);
+		txtHeight.setBounds(145, 101, 191, 28);
+		contentPane.add(txtHeight);
+		
+		JButton btnBMI = new JButton("TÍNH BMI");
+		btnBMI.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnBMI.setBounds(142, 163, 96, 35);
+		contentPane.add(btnBMI);
 	}
-
 }
