b5b5a29a97cbbd0942917ab9419521291d2211d8
Trần Văn Hậu <hautran192002@gmail.com>
2024-03-25 22:24:57 +0700

    Khởi tạo màng hình
    tạo màng hình tính toán

---
+++ ThiGK_TranVanHau_62130519/Cau1_AppBMI/src/ntu_62130519/MangHinhTinhToan.java
@@ -0,0 +1,42 @@
+package ntu_62130519;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MangHinhTinhToan extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MangHinhTinhToan frame = new MangHinhTinhToan();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MangHinhTinhToan() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
